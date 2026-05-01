691f9271389db999d3f9f9119700119580dc087a
mateusrossi-jpg <mateusrossi@icloud.com>
2026-04-30 21:58:55 -0300

    Route technical budget to human workspace

--- src/features/calculators/components/GeneralCalculatorWorkspace.ts
+++ src/features/calculators/components/GeneralCalculatorWorkspace.ts
@@ -3,6 +3,7 @@ import type { CalculationCapture } from '../../../core/types/workflow';
 import { ConstructionHumanWorkspace } from './ConstructionHumanWorkspace';
 import { GeneralCalculatorWorkspace as StableGeneralCalculatorWorkspace } from './StableGeneralCalculatorWorkspace';
 import type { GeneralCalculatorModule } from './StableGeneralCalculatorWorkspace';
+import { TechnicalBudgetHumanWorkspace } from './TechnicalBudgetHumanWorkspace';
 
 export type { GeneralCalculatorModule } from './StableGeneralCalculatorWorkspace';
 
@@ -14,6 +15,10 @@ interface GeneralCalculatorWorkspaceProps {
 export function GeneralCalculatorWorkspace({ selectedModule, onCaptureCalculation }: GeneralCalculatorWorkspaceProps) {
   if (selectedModule === 'obras') {
     return createElement(ConstructionHumanWorkspace, { onCaptureCalculation });
+  }
+
+  if (selectedModule === 'orcamentoTecnico') {
+    return createElement(TechnicalBudgetHumanWorkspace, { onCaptureCalculation });
   }
 
   return createElement(StableGeneralCalculatorWorkspace, { selectedModule, onCaptureCalculation });
